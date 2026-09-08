e16a8797aaa9d656bebcbff824dc59bbc2e90883
sanket-s11 <sanketkorde911@gmail.com>
2026-09-08 16:44:48 +0530

    Create App.java

---
+++ src/main/java/com/bookstore/App.java
@@ -0,0 +1,18 @@
+package com.bookstore;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Online Book Store");
+        System.out.println("-----------------");
+
+        System.out.println("Login successful");
+
+        System.out.println("\nAvailable Books:");
+        System.out.println("1. Java Programming");
+        System.out.println("2. Python Programming");
+        System.out.println("3. Data Structures");
+        System.out.println("4. Computer Networks");
+    }
+}
